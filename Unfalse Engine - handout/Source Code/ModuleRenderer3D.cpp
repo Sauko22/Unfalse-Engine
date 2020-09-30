765ce2ac8b8b65a7875a1b1736fe2fde5109c7e1
Sauko22 <47743898+Sauko22@users.noreply.github.com>
2020-09-30 19:23:10 +0200

    ImGui implemented

---
+++ Unfalse Engine - handout/Source Code/ModuleRenderer3D.cpp
@@ -0,0 +1,237 @@
+#include "Globals.h"
+#include "Application.h"
+#include "ModuleRenderer3D.h"
+#include "Glew\include\glew.h"
+#include "SDL\include\SDL_opengl.h"
+#include <gl/GL.h>
+#include <gl/GLU.h>
+
+#pragma comment (lib, "glu32.lib")    /* link OpenGL Utility lib     */
+#pragma comment (lib, "opengl32.lib") /* link Microsoft OpenGL lib   */
+#pragma comment (lib, "Glew/libx86/glew32.lib") /* link Microsoft OpenGL lib   */
+
+
+ModuleRenderer3D::ModuleRenderer3D(Application* app, bool start_enabled) : Module(app, start_enabled)
+{
+}
+
+// Destructor
+ModuleRenderer3D::~ModuleRenderer3D()
+{}
+
+// Called before render is available
+bool ModuleRenderer3D::Init()
+{
+	LOG("Creating 3D Renderer context");
+	bool ret = true;
+	
+	//Create context
+	context = SDL_GL_CreateContext(App->window->window);
+	if(context == NULL)
+	{
+		LOG("OpenGL context could not be created! SDL_Error: %s\n", SDL_GetError());
+		ret = false;
+	}
+	
+	GLenum error = glewInit();
+	if (error != GL_NO_ERROR)
+	{
+		LOG("Error initializing glew library ! %s", SDL_GetError());
+		ret = false;
+	}
+	else
+	{
+		LOG("using Glew %s", glewGetString(GLEW_VERSION));
+	}
+
+	if(ret == true)
+	{
+		//Use Vsync
+		if(VSYNC && SDL_GL_SetSwapInterval(1) < 0)
+			LOG("Warning: Unable to set VSync! SDL Error: %s\n", SDL_GetError());
+
+		//Initialize Projection Matrix
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+
+		//Check for error
+		GLenum error = glGetError();
+		if(error != GL_NO_ERROR)
+		{
+			LOG("Error initializing OpenGL! %s\n", gluErrorString(error));
+			ret = false;
+		}
+
+		//Initialize Modelview Matrix
+		glMatrixMode(GL_MODELVIEW);
+		glLoadIdentity();
+
+		//Check for error
+		error = glGetError();
+		if(error != GL_NO_ERROR)
+		{
+			LOG("Error initializing OpenGL! %s\n", gluErrorString(error));
+			ret = false;
+		}
+		
+		glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+		glClearDepth(1.0f);
+		
+		//Initialize clear color
+		glClearColor(0.f, 0.f, 0.f, 1.f);
+
+		//Check for error
+		error = glGetError();
+		if(error != GL_NO_ERROR)
+		{
+			LOG("Error initializing OpenGL! %s\n", gluErrorString(error));
+			ret = false;
+		}
+		
+		GLfloat LightModelAmbient[] = {0.0f, 0.0f, 0.0f, 1.0f};
+		glLightModelfv(GL_LIGHT_MODEL_AMBIENT, LightModelAmbient);
+		
+		lights[0].ref = GL_LIGHT0;
+		lights[0].ambient.Set(0.25f, 0.25f, 0.25f, 1.0f);
+		lights[0].diffuse.Set(0.75f, 0.75f, 0.75f, 1.0f);
+		lights[0].SetPos(0.0f, 0.0f, 2.5f);
+		lights[0].Init();
+		
+		GLfloat MaterialAmbient[] = {1.0f, 1.0f, 1.0f, 1.0f};
+		glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, MaterialAmbient);
+
+		GLfloat MaterialDiffuse[] = {1.0f, 1.0f, 1.0f, 1.0f};
+		glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, MaterialDiffuse);
+		
+		glEnable(GL_DEPTH_TEST);
+		glEnable(GL_CULL_FACE);
+		lights[0].Active(true);
+		glEnable(GL_LIGHTING);
+		glEnable(GL_COLOR_MATERIAL);
+	}
+
+	// Projection matrix for
+	OnResize(SCREEN_WIDTH, SCREEN_HEIGHT);
+
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+	io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
+
+	ImGui_ImplSDL2_InitForOpenGL(App->window->window,App->renderer3D->context);
+	ImGui_ImplOpenGL3_Init();
+
+	return ret;
+}
+
+// PreUpdate: clear buffer
+update_status ModuleRenderer3D::PreUpdate()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glLoadIdentity();
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadMatrixf(App->camera->GetViewMatrix());
+
+	// light 0 on cam pos
+	lights[0].SetPos(App->camera->Position.x, App->camera->Position.y, App->camera->Position.z);
+
+	for(uint i = 0; i < MAX_LIGHTS; ++i)
+		lights[i].Render();
+
+	return UPDATE_CONTINUE;
+}
+
+// PostUpdate present buffer to screen
+update_status ModuleRenderer3D::PostUpdate()
+{
+	ImGui_ImplOpenGL3_NewFrame();
+	ImGui_ImplSDL2_NewFrame(App->window->window);
+	ImGui::NewFrame();
+	//
+
+	ImGui::Begin("Test", NULL);
+	ImGui::Text("Josep menja naps com no tomorrow");
+	ImGui::End();
+
+	ImGui::Begin("Test2", NULL);
+	ImGui::Text("Josep menja naps com no tomorrow");
+	ImGui::End();
+
+	if (ImGui::BeginMainMenuBar()) {
+		if (ImGui::BeginMenu("File")) {
+			ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(0.972, 0.105, 0.105, 1.f));
+			if (ImGui::MenuItem("New")) {
+			
+				// New file
+			}
+
+			if (ImGui::MenuItem("Quit")) {
+
+				// Exits the app
+				return UPDATE_STOP;
+			}
+			ImGui::PopStyleColor();
+			ImGui::EndMenu();
+		}
+		if (ImGui::BeginMenu("Help")) {
+			ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(0.972, 0.105, 0.105, 1.f));
+			if (ImGui::MenuItem("Our Github")) {
+
+				// New file
+			}
+
+			
+			ImGui::PopStyleColor();
+			ImGui::EndMenu();
+		}
+	
+	
+	
+		ImGui::EndMainMenuBar();
+	}
+
+	//Rendering
+
+	ImGui::Render();
+	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+	{
+		SDL_Window* backup_current_window = SDL_GL_GetCurrentWindow();
+		SDL_GLContext backup_current_context = SDL_GL_GetCurrentContext();
+
+		ImGui::UpdatePlatformWindows();
+		ImGui::RenderPlatformWindowsDefault();
+		SDL_GL_MakeCurrent(backup_current_window, backup_current_context);
+
+	}
+	SDL_GL_SwapWindow(App->window->window);
+	return UPDATE_CONTINUE;
+}
+
+// Called before quitting
+bool ModuleRenderer3D::CleanUp()
+{
+	LOG("Destroying 3D Renderer");
+
+	SDL_GL_DeleteContext(context);
+
+	return true;
+}
+
+
+void ModuleRenderer3D::OnResize(int width, int height)
+{
+	glViewport(0, 0, width, height);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	ProjectionMatrix = perspective(60.0f, (float)width / (float)height, 0.125f, 512.0f);
+	glLoadMatrixf(&ProjectionMatrix);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
---
+++ Unfalse Engine - handout/Source Code/ModuleRenderer3D.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "Module.h"
+#include "Globals.h"
+#include "glmath.h"
+#include "Light.h"
+
+
+//ImGui includes
+#include "Imgui/imgui.h"
+#include "Imgui/imgui_internal.h"
+#include "Imgui/imgui_impl_sdl.h"
+#include "Imgui/imgui_impl_opengl3.h"
+
+#define MAX_LIGHTS 8
+
+class ModuleRenderer3D : public Module
+{
+public:
+	ModuleRenderer3D(Application* app, bool start_enabled = true);
+	~ModuleRenderer3D();
+
+	bool Init();
+	update_status PreUpdate();
+	update_status PostUpdate();
+	bool CleanUp();
+
+	void OnResize(int width, int height);
+
+public:
+
+	Light lights[MAX_LIGHTS];
+	SDL_GLContext context;
+	mat3x3 NormalMatrix;
+	mat4x4 ModelMatrix, ViewMatrix, ProjectionMatrix;
+};
