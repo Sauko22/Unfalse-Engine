9c3cedaea34dcc586806cfe1619451031b71f4c8
marcpt98 <47535763+marcpt98@users.noreply.github.com>
2020-10-26 23:53:20 +0100

    hierarchy gameobject selection fixed

--- Unfalse Engine - handout/Source Code/ModuleUI.cpp
+++ Unfalse Engine - handout/Source Code/ModuleUI.cpp
@@ -283,60 +283,48 @@ update_status ModuleUI::Update()
 
 		ImGui::End();
 	}
-	if (ImGui::Begin("Hierarchy", NULL)) 
+
+	if (ImGui::Begin("Hierarchy"))
 	{
-		//ImGui::TextColored({ 1.0f, 1.0f, 0.0f, 1.0f }, " GameObject %s", App->gameobject->gameobject_list[i]->name.c_str());
-		
-		for (int i = 0; i < App->gameobject->gameobject_list.size(); i++) {
-			
-			static int selection_mask = (1 << 2);
-			int node_clicked = -1;
-			static ImGuiTreeNodeFlags base_flags = ImGuiTreeNodeFlags_OpenOnArrow | ImGuiTreeNodeFlags_OpenOnDoubleClick | ImGuiTreeNodeFlags_SpanAvailWidth;
-			static bool test_drag_and_drop = false;
-			
-				// Disable the default "open on single-click behavior" + set Selected flag according to our selection.
-				ImGuiTreeNodeFlags node_flags = base_flags;
-				const bool is_selected = (selection_mask & (1 << i)) != 0;
-				if (is_selected)
-					node_flags |= ImGuiTreeNodeFlags_Selected;
+		static ImGuiTreeNodeFlags base_flags = ImGuiTreeNodeFlags_OpenOnArrow | ImGuiTreeNodeFlags_OpenOnDoubleClick | ImGuiTreeNodeFlags_SpanAvailWidth;
+		static int selection_mask = (1 << 2);
+		int node_clicked = -1;
+		for (int i = 0; i < App->gameobject->gameobject_list.size(); i++) 
+		{
+			// Disable the default "open on single-click behavior" + set Selected flag according to our selection.
+			ImGuiTreeNodeFlags node_flags = base_flags;
+			const bool is_selected = (selection_mask & (1 << i)) != 0;
+			if (is_selected)
+				node_flags |= ImGuiTreeNodeFlags_Selected;
 				
-					// Items 0..2 are Tree Node
-					bool node_open = ImGui::TreeNodeEx((void*)(intptr_t)i, node_flags, "GameObject %s", App->gameobject->gameobject_list[i]->name.c_str());
-					if (ImGui::IsItemClicked())
-						node_clicked = i;
-					
-					if (node_open)
-					{
-						for (int j = 0; j < App->gameobject->gameobject_list[i]->comp_list.size(); j++) {
-							node_flags |= ImGuiTreeNodeFlags_Leaf | ImGuiTreeNodeFlags_NoTreePushOnOpen; // ImGuiTreeNodeFlags_Bullet
-							ImGui::TreeNodeEx((void*)(intptr_t)j, node_flags, "Components %s", App->gameobject->gameobject_list[i]->comp_list[j]->name.c_str());
-							/*ImGui::TextColored({ 1.0f, 1.0f, 0.0f, 1.0f }, "Components %s", App->gameobject->gameobject_list[i]->comp_list[j]->name.c_str());*/
-						}
-						ImGui::TreePop();
-					}
-				
-				//else
-				//{
-				//	// Items 3..5 are Tree Leaves
-				//	// The only reason we use TreeNode at all is to allow selection of the leaf. Otherwise we can
-				//	// use BulletText() or advance the cursor by GetTreeNodeToLabelSpacing() and call Text().
-				//	node_flags |= ImGuiTreeNodeFlags_Leaf | ImGuiTreeNodeFlags_NoTreePushOnOpen; // ImGuiTreeNodeFlags_Bullet
-				//	ImGui::TreeNodeEx((void*)(intptr_t)i, node_flags, "Selectable Leaf %d", i);
-				//	if (ImGui::IsItemClicked())
-				//		node_clicked = i;
-				//	if (test_drag_and_drop && ImGui::BeginDragDropSource())
-				//	{
-				//		ImGui::SetDragDropPayload("_TREENODE", NULL, 0);
-				//		ImGui::Text("This is a drag and drop source");
-				//		ImGui::EndDragDropSource();
-				//	}
-				//}
-			
-			
+			bool node_open = ImGui::TreeNodeEx((void*)(intptr_t)i, node_flags, "GameObject %s", App->gameobject->gameobject_list[i]->name.c_str());
+			if (ImGui::IsItemClicked())
+				node_clicked = i;
+
+			if (node_open)
+			{
+				// Items 0..2 are Tree Node
+				for (int j = 0; j < App->gameobject->gameobject_list[i]->comp_list.size(); j++)
+				{
+					node_flags |= ImGuiTreeNodeFlags_Leaf | ImGuiTreeNodeFlags_NoTreePushOnOpen; // ImGuiTreeNodeFlags_Bullet
+					ImGui::TreeNodeEx((void*)(intptr_t)j, node_flags, "Components %s", App->gameobject->gameobject_list[i]->comp_list[j]->name.c_str());
+				}
+				ImGui::TreePop();
+			}
 		}
+		if (node_clicked != -1)
+		{
+			// Update selection state
+			// (process outside of tree loop to avoid visual inconsistencies during the clicking frame)
+			if (ImGui::GetIO().KeyCtrl)
+				selection_mask ^= (1 << node_clicked);          // CTRL+click to toggle
+			else //if (!(selection_mask & (1 << node_clicked))) // Depending on selection behavior you want, may want to preserve selection when clicking on item that is part of the selection
+				selection_mask = (1 << node_clicked);           // Click to single-select
+		}
+
 		ImGui::End();
-		
 	}
+
 	if (ImGui::Begin("Console", NULL)) 
 	{
 		showConsoleWin();
